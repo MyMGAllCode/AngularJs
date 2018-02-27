c30d2744bae9cb4881da0e56eeab158ae3ebf393
dktech89 <35030016+dktech89@users.noreply.github.com>
2018-02-27 23:53:40 +0530

    Add files via upload
    commit

---
+++ AJSProj-3DynamicBySeprating-Ctrl-app/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by DKP on 24-Dec-16.
+ */
+var app=angular.module("app",[]);
---
+++ AJSProj-3DynamicBySeprating-Ctrl-app/controller/ctrl.js
@@ -0,0 +1,12 @@
+/**
+ * Created by DKP on 24-Dec-16.
+ */
+app.controller("ctrl", ctrl_fun);
+
+
+ctrl.$inject=['$scope'];
+
+function ctrl_fun($scope) {
+    $scope.val_one="This Is Dharmendra";
+
+}
